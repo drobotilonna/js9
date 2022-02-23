44440ac5a3f03946a03df4c55c28cb74bcb59f18
Ilona Drobot <drobotilonna@gmail.com>
2022-02-23 17:40:59 +0200

    homework9

--- script.js
+++ script.js
@@ -1,5 +1,35 @@
-let st =true
-while(st ==true){
-    console.log("uuuuu...")
-    st=confirm('продолжыть?')
+//let st =true
+//while(st ==true){
+   // console.log("uuuuu...")
+    //st=confirm('продолжыть?')
+//}
+
+
+//let a = Number (prompt('Ddelbnt xbckj'))
+//let b = Number (prompt('Ddelbnt xbckj'))
+
+//if(a+b<20){
+ //  while (a+b<20) {
+ //       let a = Number (prompt('Ddelbnt'))
+  //      let b = Number (prompt('Ddelbnt '))
+ //   }
+//}else{
+  //  console.log(a+b)
+//}
+let a = true
+do{
+let i=0
+let b=0
+ i = Number (prompt('Ddelbnt xbckj'))
+ b = Number (prompt('Ddelbnt xbckj'))
+ if((i+b)>20){
+    a=false
 }
+}
+
+
+while (a==true) {
+    console.log(a+b)
+    a = Number (prompt("Dtlbnt"))
+}
+
